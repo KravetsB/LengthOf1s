5b38694b4a87a846abb424bcd4e2e1fee85f068f
ProgrammerBK <40270763+ProgrammerBK@users.noreply.github.com>
2019-02-21 19:10:09 +0200

    Add files via upload

---
+++ LengthOf1s/LengthOf1s/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace AlgorithmForTheTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            using (StreamReader reader = new StreamReader("input.txt", Encoding.Default))
+                str = reader.ReadToEnd();
+
+            char[] array = new char[str.Length];
+            array = str.ToCharArray();
+
+            Console.WriteLine(str);
+            Console.WriteLine(str.Length);
+
+
+            int count = 0;
+            int temp = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (array[i] == '1')
+                {
+                    count++;
+                    if (i == str.Length - 1)
+                        temp = count;
+                }
+                else
+                {
+                    if (count > temp)
+                    {
+                        temp = count;
+
+                    }
+                    count = 0;
+                }
+            }
+
+
+
+            FileStream file1 = new FileStream("output.txt", FileMode.Create); // створюємо файловий потік
+            StreamWriter writer = new StreamWriter(file1); // створюємо «потоковий письменник» і пов'язуємо його з файловим потоком
+            writer.Write(temp); // записуємо в файл
+            writer.Close(); // закриваємо потік. Чи не закривши потік, у файл нічого не запишеться
+            Console.WriteLine(temp);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
